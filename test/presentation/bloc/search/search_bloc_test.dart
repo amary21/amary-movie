d2935a78466d4cfaf9ca8690a642b0f4ed1265c9
T-Amary <taufik.amary@gmail.com>
2025-04-20 00:49:53 +0700

    test: add unit tests for SearchBloc and WatchlistBloc with mock implementations

---
+++ test/presentation/bloc/search/search_bloc_test.dart
@@ -0,0 +1,78 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/catalog.dart';
+import 'package:ditonton/domain/usecases/search_catalog.dart';
+import 'package:ditonton/presentation/bloc/search/search_bloc.dart';
+import 'package:ditonton/presentation/bloc/search/search_event.dart';
+import 'package:ditonton/presentation/bloc/search/search_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import 'search_bloc_test.mocks.dart';
+
+@GenerateMocks([SearchCatalog])
+void main() {
+  late SearchBloc searchBloc;
+  late MockSearchCatalog mockSearchCatalog;
+
+  setUp(() {
+    mockSearchCatalog = MockSearchCatalog();
+    searchBloc = SearchBloc(mockSearchCatalog);
+  });
+
+  const testQuery = 'avengers';
+
+  test('initial state should be SearchEmpty', () {
+    expect(searchBloc.state, SearchEmpty());
+  });
+
+  blocTest<SearchBloc, SearchState>(
+    'emits [SearchLoading, SearchHasData] when search is successful',
+    build: () {
+      when(
+        mockSearchCatalog.execute(Catalog.movie, testQuery),
+      ).thenAnswer((_) async => Right(testCatalogItemList));
+      return searchBloc;
+    },
+    act: (bloc) => bloc.add(const OnQueryChanged(testQuery, Catalog.movie)),
+    expect: () => [SearchLoading(), SearchHasData(testCatalogItemList)],
+    verify: (_) {
+      verify(mockSearchCatalog.execute(Catalog.movie, testQuery)).called(1);
+    },
+  );
+
+  blocTest<SearchBloc, SearchState>(
+    'emits [SearchLoading, SearchError] when search fails',
+    build: () {
+      when(
+        mockSearchCatalog.execute(Catalog.tv, testQuery),
+      ).thenAnswer((_) async => Left(ServerFailure('Server Error')));
+      return searchBloc;
+    },
+    act: (bloc) => bloc.add(const OnQueryChanged(testQuery, Catalog.tv)),
+    expect: () => [SearchLoading(), SearchError('Server Error')],
+    verify: (_) {
+      verify(mockSearchCatalog.execute(Catalog.tv, testQuery)).called(1);
+    },
+  );
+
+  blocTest<SearchBloc, SearchState>(
+    'emits [SearchEmpty] when reset is triggered',
+    build: () => searchBloc,
+    act: (bloc) => bloc.add(OnResetSearch()),
+    expect: () => [SearchEmpty()],
+  );
+
+  test('SearchEvent props should match the values', () {
+    final event = OnQueryChanged('test query', Catalog.tv);
+
+    expect(event.props, ['test query', Catalog.tv]);
+  });
+
+  test('OnResetSearch supports value comparison', () {
+    expect(OnResetSearch(), equals(OnResetSearch()));
+  });
+}
---
+++ test/presentation/bloc/watchlist/watchlist_bloc_test.dart
@@ -0,0 +1,70 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/catalog.dart';
+import 'package:ditonton/domain/usecases/get_watchlist_catalog.dart';
+import 'package:ditonton/presentation/bloc/watchlist/watchlist_bloc.dart';
+import 'package:ditonton/presentation/bloc/watchlist/watchlist_event.dart';
+import 'package:ditonton/presentation/bloc/watchlist/watchlist_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import 'watchlist_bloc_test.mocks.dart';
+
+class DummyWatchlistEvent extends WatchlistEvent {}
+
+@GenerateMocks([GetWatchlistCatalog])
+void main() {
+  late WatchlistBloc watchlistBloc;
+  late MockGetWatchlistCatalog mockGetWatchlistCatalog;
+
+  setUp(() {
+    mockGetWatchlistCatalog = MockGetWatchlistCatalog();
+    watchlistBloc = WatchlistBloc(mockGetWatchlistCatalog);
+  });
+
+  test('initial state should be WatchlistEmpty', () {
+    expect(watchlistBloc.state, WatchlistEmpty());
+  });
+
+  blocTest<WatchlistBloc, WatchlistState>(
+    'emits [WatchlistLoading, WatchlistHasData] when fetch is successful',
+    build: () {
+      when(
+        mockGetWatchlistCatalog.execute(Catalog.movie),
+      ).thenAnswer((_) async => Right(testCatalogItemList));
+      return watchlistBloc;
+    },
+    act: (bloc) => bloc.add(FetchWatchlist(Catalog.movie)),
+    expect: () => [WatchlistLoading(), WatchlistHasData(testCatalogItemList)],
+    verify: (_) {
+      verify(mockGetWatchlistCatalog.execute(Catalog.movie)).called(1);
+    },
+  );
+
+  blocTest<WatchlistBloc, WatchlistState>(
+    'emits [WatchlistLoading, WatchlistError] when fetch fails',
+    build: () {
+      when(
+        mockGetWatchlistCatalog.execute(Catalog.tv),
+      ).thenAnswer((_) async => Left(ServerFailure('Server Error')));
+      return watchlistBloc;
+    },
+    act: (bloc) => bloc.add(FetchWatchlist(Catalog.tv)),
+    expect: () => [WatchlistLoading(), WatchlistError('Server Error')],
+    verify: (_) {
+      verify(mockGetWatchlistCatalog.execute(Catalog.tv)).called(1);
+    },
+  );
+
+  test('FetchWatchlist props should match the values', () {
+    final event = FetchWatchlist(Catalog.tv);
+    expect(event.props, [Catalog.tv]);
+  });
+
+  test('WatchlistEvent default supports value comparison', () {
+    expect(DummyWatchlistEvent(), equals(DummyWatchlistEvent()));
+  });
+}
